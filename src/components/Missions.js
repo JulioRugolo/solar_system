250faf1ee707197cf617b627be6651179eeee505
Julio Rugoloe <juliorugolo@gmail.com>
2023-03-01 16:56:16 -0300

    requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map(({ name, year, country, destination }) => (
+            <MissionCard
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+              key={ name }
+            />
+          ))
+        }
       </div>
     );
   }
