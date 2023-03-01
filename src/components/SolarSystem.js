100e316fdbfb18ad456f13723a942eda4f9aeb5a
Julio Rugoloe <juliorugolo@gmail.com>
2023-03-01 16:44:13 -0300

    fix: SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import Planets from './Planets';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div>
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <Planets />
+        {
+          Planets.map(({ name, image }) => (
+            <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+          ))
+        }
       </div>
     );
   }
